1c3df7542a445c5bdf6804dbbec658d352f7b99d
Zacarias <zaca03zaca@gmail.com>
2024-06-16 20:48:18 -0300

    Add tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+//** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      display: ["Poppins"],
+    },
+    extend: {
+      fontFamily: {
+        VT323: ['"VT323"', "monospace"],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "retro", "cyberpunk", "valentine", "aqua"],
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
+}
