d70737b2620271b460aef6b7a03fdf94ba2502ba
Zacarias <zaca03zaca@gmail.com>
2024-06-16 20:52:27 -0300

    Add theme controller component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import ThemeController from "./components/ThemeController"
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <ThemeController/>
     </>
   )
 }
