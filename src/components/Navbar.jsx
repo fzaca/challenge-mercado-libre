7f9337f7d77e75df18d1fad27ff3eb2f81d71434
Zacarias <zaca03zaca@gmail.com>
2024-06-16 22:04:02 -0300

    Integrate ThemeController into Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
 import SearchBar from './SearchBar';
+import ThemeController from "../components/ThemeController";
 
 const Navbar = ({ onSearch }) => {
   const { cart } = useContext(CartContext);
@@ -26,9 +27,7 @@ const Navbar = ({ onSearch }) => {
         <SearchBar onSearch={onSearch} />
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-        </button>
+        <ThemeController />
         <div className="flex-none">
           <div className="dropdown dropdown-end">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
