2680b4810e66d78d1c2b43e655ece4fe18c1ccd4
Zacarias <zaca03zaca@gmail.com>
2024-06-16 21:12:43 -0300

    Configure routing and add new routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import ThemeController from "./components/ThemeController"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import ThemeController from "./components/ThemeController";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <ThemeController/>
-    </>
-  )
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <ThemeController />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
