d9f4d31928b788c1799564e1229e8f6ebc6fbdb7
Zacarias <zaca03zaca@gmail.com>
2024-06-16 21:37:09 -0300

    Integrate SearchBar component into Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
+import SearchBar from './SearchBar';
 
 const Navbar = ({ onSearch }) => {
   const { cart } = useContext(CartContext);
   const totalItems = cart.length;
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
-
-  const [query, setQuery] = useState('');
-
-  const handleSearch = () => {
-    onSearch(query);
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
-  };
 
   return (
     <div className="navbar bg-base-100">
@@ -34,25 +23,7 @@ const Navbar = ({ onSearch }) => {
         </div>
       </div>
       <div className="navbar-center">
-        <label className="input input-bordered flex items-center gap-2">
-          <input 
-            type="text" 
-            className="grow" 
-            value={query}
-            onChange={(e) => setQuery(e.target.value)} 
-            onKeyPress={handleKeyPress}
-            placeholder="Search" 
-          />
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 16 16" 
-            fill="currentColor" 
-            className="w-4 h-4 opacity-70 cursor-pointer"
-            onClick={handleSearch}
-          >
-            <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
-          </svg>
-        </label>
+        <SearchBar onSearch={onSearch} />
       </div>
       <div className="navbar-end">
         <button className="btn btn-ghost btn-circle">
