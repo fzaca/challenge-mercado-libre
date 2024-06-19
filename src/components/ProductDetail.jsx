553749092356d429140ec87d2d230cdabf386b95
Zacarias <zaca03zaca@gmail.com>
2024-06-19 18:14:35 -0300

    Fix words

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -48,7 +48,7 @@ const ProductDetail = () => {
           <h1 className="card-title text-2xl font-bold prose">{product.title}</h1>
           <p className="text-lg text-gray-700">${product.price.toFixed(2)}</p>
           <div className="flex items-center mb-4">
-            <span className="mr-2">Cantidad:</span>
+            <span className="mr-2">Quantity:</span>
             <input 
               type="number" 
               min="1" 
@@ -62,7 +62,7 @@ const ProductDetail = () => {
             onClick={() => addToCart(product, quantity)} 
             className="btn btn-primary mb-4"
           >
-            Añadir al carrito
+            Add to cart
           </button>
           <div className="text-gray-600 mb-4 max-h-[600px] overflow-y-auto p-2 rounded prose">
             {description}
@@ -72,21 +72,21 @@ const ProductDetail = () => {
               <div className="stat-figure text-primary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
               </div>
-              <div className="stat-title">Condición</div>
+              <div className="stat-title">Condition</div>
               <div className="stat-value">{product.condition === 'new' ? 'Nuevo' : 'Usado'}</div>
             </div>
             <div className="stat">
               <div className="stat-figure text-secondary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
               </div>
-              <div className="stat-title">Disponibilidad</div>
+              <div className="stat-title">Availability</div>
               <div className="stat-value">{product.available_quantity}</div>
             </div>
             <div className="stat">
               <div className="stat-figure text-secondary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
               </div>
-              <div className="stat-title">Vendidos</div>
+              <div className="stat-title">Sold</div>
               <div className="stat-value">{product.sold_quantity}</div>
             </div>
           </div>
@@ -96,7 +96,7 @@ const ProductDetail = () => {
               <div className="stat-figure text-secondary">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
               </div>
-              <div className="stat-title">Ubicación</div>
+              <div className="stat-title">Ubication</div>
               <div className="stat-value">{product.seller_address.city.name}, {product.seller_address.state.name}</div>
             </div>
           </div>
